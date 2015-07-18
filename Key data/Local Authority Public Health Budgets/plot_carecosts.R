4b2f911a67bbb568e1ad494dfe6d0dd522d66bfe
Giovanni M Dall'Olio <giovanni.dallolio@upf.edu>
2015-07-18 12:05:21 +0100

    CARE COSTS: plotting districts with top difference

--- Key data/Local Authority Public Health Budgets/plot_carecosts.R
+++ Key data/Local Authority Public Health Budgets/plot_carecosts.R
@@ -8,10 +8,12 @@ pdf("care_costs.pdf")
 # plot comparison residential vs at home cost
 care.costs %>% gather(variable, cost, -District, -Postcode) %>% group_by(variable) %>% ggplot(aes(x=variable, y= cost)) + geom_boxplot() + ggtitle("Cost of Residential Care vs At Home Care")
 
-# plot comparison of residential vs at home by the first three letters of postcode
-care.costs %>% gather(variable, cost, -District, -Postcode) %>% separate(Postcode, into=c("postcode1", "postcode2"), sep=" ") %>% group_by(variable) %>% ggplot(aes(x=variable, y= cost)) + geom_boxplot() + ggtitle("Cost of Residential Care vs At Home Care") + facet_wrap(~postcode1)
+## plot comparison of residential vs at home by the first three letters of postcode
+#care.costs %>% gather(variable, cost, -District, -Postcode) %>% separate(Postcode, into=c("postcode1", "postcode2"), sep=" ") %>% group_by(variable) %>% ggplot(aes(x=variable, y= cost)) + geom_boxplot() + ggtitle("Cost of Residential Care vs At Home Care") + facet_wrap(~postcode1)
 
-
+# Top 10 district with higher difference
+ care.costs %>% mutate(difference = Residential.care - At.home.care) %>% arrange(desc(abs(difference))) %>% head(10) %>% gather(variable, cost, -District, -difference, -Postcode) %>% group_by(variable) %>% ggplot(aes(x=variable, y=cost, fill=cost)) + geom_bar(stat='identity') + ggtitle(" Districts with higher absolute difference between At Home Costs and Residential Care") + facet_wrap(~District)
+ care.costs %>% mutate(difference = Residential.care - At.home.care) %>% arrange(abs(difference)) %>% head(10) %>% gather(variable, cost, -District, -difference, -Postcode) %>% group_by(variable) %>% ggplot(aes(x=variable, y=cost, fill=cost)) + geom_bar(stat='identity') + ggtitle(" Districts with higher absolute difference between At Home Costs and Residential Care") + facet_wrap(~District)
 
 dev.off()
 
